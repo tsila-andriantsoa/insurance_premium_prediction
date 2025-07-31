d0ec63bb33bc97c2083d28266d236a6fa79206a3
tsila-andriantsoa <tsilavinau@gmail.com>
2025-07-31 15:10:37 +0000

    review train and data preparation flow

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,95 +1,105 @@
 import numpy as np
 import pandas as pd
+import os
 
 from sklearn.model_selection import train_test_split
-
-# Load datasets
-# train = pd.read_csv('../data/raw/train.csv', )
-# test = pd.read_csv('../data/raw/test.csv', )
-
-train = pd.read_parquet('../data/raw/train.parquet')
-test = pd.read_parquet('../data/raw/test.parquet')
-
-print(train.shape, test.shape)
-
-# rename columns
-train.columns = [str.lower(col).replace(' ','_') for col in train.columns]
-
-# remove null value
-train.dropna(subset='premium_amount',inplace=True)
-# remove unused columns
-train.drop(columns=['id'], inplace = True)
-train.drop(columns = ['previous_claims', 'occupation'], inplace = True)
-
-numerical_features = train.select_dtypes(include = 'float').columns.tolist()
-numerical_features = [n for n in numerical_features if n != 'premium_amount']
-categorical_features = train.select_dtypes(include = 'object').columns.tolist()
-categorical_features = [n for n in categorical_features if n != 'policy_start_date']
-
-numerical_features = [n for n in numerical_features if n != 'previous_claims']
-categorical_features = [n for n in categorical_features if n != 'occupation']
 
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.impute import SimpleImputer
 
-# Preprocessing for numerical data
-numerical_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='median')), 
-    ('scaler', StandardScaler())
-])
+RAW_DATA_PATH = "data/raw/"
+PREPARED_DATA_PATH = "data/prepared/"
+os.makedirs(PREPARED_DATA_PATH, exist_ok=True)
 
-# Preprocessing for categorical data
-categorical_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='most_frequent')),  
-    ('onehot', OneHotEncoder(drop = 'first', handle_unknown='ignore'))
-])
+def load_raw_data():
+    train = pd.read_parquet(RAW_DATA_PATH + 'train.parquet')
+    test = pd.read_parquet(RAW_DATA_PATH + 'test.parquet')
+    print("raw data loaded")
+    return train, test
 
-# Combine preprocessors in a ColumnTransformer
-preprocessor = ColumnTransformer(
-    transformers=[
-        ('num', numerical_transformer, numerical_features),
-        ('cat', categorical_transformer, categorical_features)
+def preprocess_data(df):
+
+    # rename columns    
+    df.columns = [str.lower(col).replace(' ','_') for col in df.columns]
+
+    # remove null value
+    df.dropna(subset='premium_amount',inplace=True)
+
+    # remove unused columns
+    df.drop(columns=['id'], inplace = True)
+    df.drop(columns = ['previous_claims', 'occupation'], inplace = True)
+
+    print("train data preprocessed")
+    return df
+
+def prepared_train_validation_data(df):
+
+    numerical_features = df.select_dtypes(include = 'float').columns.tolist()
+    numerical_features = [n for n in numerical_features if n != 'premium_amount' if n!= 'previous_claims']
+    categorical_features = df.select_dtypes(include = 'object').columns.tolist()
+    categorical_features = [n for n in categorical_features if n != 'policy_start_date' if n!= 'occupation']
+
+    # Preprocessing for numerical data
+    numerical_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='median')), 
+        ('scaler', StandardScaler())
     ])
 
-pipeline = Pipeline([
-    ('preprocessor',  preprocessor)
-])
+    # Preprocessing for categorical data
+    categorical_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='most_frequent')),  
+        ('onehot', OneHotEncoder(drop = 'first', handle_unknown='ignore'))
+    ])
 
-from sklearn.feature_selection import mutual_info_classif
+    # Combine preprocessors in a ColumnTransformer
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', numerical_transformer, numerical_features),
+            ('cat', categorical_transformer, categorical_features)
+        ])
 
-X_train = train.drop(columns = ['premium_amount'])
-Y_train = train['premium_amount']
+    pipeline = Pipeline([
+        ('preprocessor',  preprocessor)
+    ])
 
-pipeline.fit(X_train, Y_train)
+    X_train = df.drop(columns = ['premium_amount'])
+    Y_train = df['premium_amount']
 
-X_train_transformed = pipeline.named_steps['preprocessor'].transform(X_train)
-numerical_transformed_columns = pipeline.named_steps['preprocessor'].transformers_[0][2]
-categorical_transformed_columns = pipeline.named_steps['preprocessor'].transformers_[1][1].get_feature_names_out().tolist()
-all_columns = [numerical_transformed_columns + categorical_transformed_columns]
-all_columns = all_columns[0]
+    pipeline.fit(X_train, Y_train)
 
-all_columns = [str.lower(col).replace('\'','').replace(' ','_') for col in all_columns]
+    X_train_transformed = pipeline.named_steps['preprocessor'].transform(X_train)
+    numerical_transformed_columns = pipeline.named_steps['preprocessor'].transformers_[0][2]
+    categorical_transformed_columns = pipeline.named_steps['preprocessor'].transformers_[1][1].get_feature_names_out().tolist()
+    all_columns = [numerical_transformed_columns + categorical_transformed_columns]
+    all_columns = all_columns[0]
 
-df_X_full_train_transformed = pd.DataFrame(
-    X_train_transformed,
-    columns = all_columns
-)
+    all_columns = [str.lower(col).replace('\'','').replace(' ','_') for col in all_columns]
 
-# select best columns
-top_features = ['credit_score', 'customer_feedback_good', 'annual_income', 'health_score']
+    df_X_full_train_transformed = pd.DataFrame(
+        X_train_transformed,
+        columns = all_columns
+    )
 
-X_train_part, X_validation_part, y_train_part, y_validation_part = train_test_split(df_X_full_train_transformed[top_features], Y_train, test_size=0.2, random_state=42)
+    # select best columns
+    top_features = ['credit_score', 'customer_feedback_good', 'annual_income', 'health_score']
 
-# save data
-df_training = X_train_part.copy()
-df_training['premium_amount'] = y_train_part.values  # Or just y_training_part if the index aligns
+    X_train_part, X_validation_part, y_train_part, y_validation_part = train_test_split(df_X_full_train_transformed[top_features], Y_train, test_size=0.2, random_state=42)
 
-df_validation = X_validation_part.copy()
-df_validation['premium_amount'] = y_validation_part.values  # Or just y_training_part if the index aligns
+    # save data
+    df_training = X_train_part.copy()
+    df_training['premium_amount'] = y_train_part.values  # Or just y_training_part if the index aligns
 
-df_training.to_parquet('../data/prepared/df_training.parquet', index=False)
-df_validation.to_parquet('../data/prepared/df_validation.parquet', index=False)
+    df_validation = X_validation_part.copy()
+    df_validation['premium_amount'] = y_validation_part.values  # Or just y_training_part if the index aligns
 
-print(df_training.shape, df_validation.shape)
+    df_training.to_parquet(PREPARED_DATA_PATH + 'df_training.parquet', index=False)
+    df_validation.to_parquet(PREPARED_DATA_PATH + 'df_validation.parquet', index=False)
+    print('train data and validation data saved into path: ', PREPARED_DATA_PATH)
+
+
+if __name__ == '__main__':
+    df_train, df_test = load_raw_data()
+    df_train_preprocessed = preprocess_data(df_train)
+    prepared_train_validation_data(df_train_preprocessed) 
--- src/train.py
+++ src/train.py
@@ -8,9 +8,72 @@ import xgboost as xgb
 from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
 from hyperopt.pyll import scope
 
+import os
+
+RAW_DATA_PATH = "data/raw/"
+PREPARED_DATA_PATH = "data/prepared/"
+
+
 # Define evaluation metrics
 def rmsle(y_true, y_pred):
     # Clip predictions to avoid log(0)
     y_pred = np.maximum(0, y_pred)
     return np.sqrt(mean_squared_log_error(y_true, y_pred))
+
+def train():
+
+    df_train = pd.read_parquet(PREPARED_DATA_PATH + 'df_training.parquet')
+    df_validation = pd.read_parquet(PREPARED_DATA_PATH+ 'df_validation.parquet')
+
+    X_train_part = df_train.drop(columns = ['premium_amount'])
+    y_train_part = df_train[['premium_amount']]
+
+    X_validation_part = df_validation.drop(columns = ['premium_amount'])
+    y_validation_part = df_validation[['premium_amount']]
+
+    train = xgb.DMatrix(X_train_part, label=y_train_part)
+    valid = xgb.DMatrix(X_validation_part, label=y_validation_part)
+
+    import mlflow
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_experiment('insurance-premium-experiment')
+
+    params = {
+        'max_depth': scope.int(hp.quniform('max_depth', 4, 50, 1)),
+        'learning_rate': hp.loguniform('learning_rate', -3, 0),
+        'min_child_weight': hp.loguniform('min_child_weight', -1, 3),
+        'objective': 'reg:linear',
+        'seed': 42
+    }
+
+    with mlflow.start_run():
+        mlflow.set_tag("model", "xgboost")
+        mlflow.log_params(params)        
+        booster = xgb.train(
+            params=params,
+            dtrain=train,
+            num_boost_round=1000,
+            evals=[(valid, 'validation')],
+            early_stopping_rounds=10
+        )
+        y_pred = booster.predict(valid)
+        rmse = rmsle(y_validation_part, y_pred)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.sklearn.log_model(booster, "xgb_model")
+
+best_result = fmin(
+    fn=objective,
+    space=search_space,
+    algo=tpe.suggest,
+    max_evals=10,
+    trials=Trials()
+)
+
+best_parameters = {
+    'max_depth': 6,
+    'learning_rate': 0.1967,
+    'min_child_weight': 1.8356,
+    'objective': 'reg:linear',
+    'seed': 42
+}
 
